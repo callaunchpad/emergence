4b593d6dc18b6c8a83a1cc70223373301212d822
jhejna <joey.hejna@gmail.com>
2019-12-26 00:51:34 -0800

    package structure

---
+++ experiments/MM_DD_YY/example.sh
@@ -0,0 +1,3 @@
+cd ../scripts
+
+python train_model.py --env <ENV> --alg <ALG> --layers 64 64 ... etc
---
+++ setup.py
@@ -0,0 +1,32 @@
+import sys, os
+from setuptools import setup, find_packages
+
+'''
+Symmetric Play Setup Script
+
+Notes for later: see package_data arg if additional files need to be supplied.
+
+'''
+if sys.version_info.major != 3:
+    print('Please use Python3!')
+
+setup(name='symmetric_play',
+        packages=[package for package in find_packages()
+                    if package.startswith('symmetric_play')],
+        install_requires=[
+            'gym[atari,classic_control]==',
+            'stable_baselines[mpi]==2.9.0',
+            'matplotlib'],
+        extras_require={
+            'cpu' : ['tensorflow==1.14.0'],
+            'gpu' : ['tensorflow-gpu==1.14.0'],
+            },
+        description='Framework for Symmetric Play Experiments',
+        author='Joey Hejna',
+        url='https://github.com/jhejna/symmetric_play',
+        lisence='MIT',
+        version='0.0.1',
+        )
+        
+            
+
