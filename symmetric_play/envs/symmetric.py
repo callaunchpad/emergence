b151b338b97380eea1294b72a66bb74aa30a3f70
Suhn Hyoung Kim <suhnkim@gmail.com>
2020-03-01 12:59:18 -0800

    fix merge

--- symmetric_play/envs/symmetric.py
+++ symmetric_play/envs/symmetric.py
@@ -9,12 +9,8 @@ class SymmetricGame(ABC, gym.Env):
     @abstractmethod
     def step(self, actions):
         '''
-<<<<<<< HEAD
-        Moves the players in the game according to list of "actions".
-=======
         Moves all players in the game. actions should be a matrix of size (N, A, D)
         N = batch, A = number of agents, D = dimension of action space.
->>>>>>> 56ad40bc9763bf9c409c58d4fbb5d46697054eab
         Returns:
 			state - state of the environment
             reward - rewards the players gets from their actions.
