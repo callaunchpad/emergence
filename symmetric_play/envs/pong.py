3e31e3bc1fd7e9601f1a9649568fb22978049dd0
Chi-Feng Wang <reinaw1012@berkeley.edu>
2020-04-06 19:21:05 -0700

    fixed bugs

--- symmetric_play/envs/pong.py
+++ symmetric_play/envs/pong.py
@@ -11,7 +11,6 @@ class PongObject:
     def __init__(self, pos, vel):
         self.pos = np.array(pos)
         self.vel = np.array(vel)
-        self.score = 0
 
 class Pong(gym.Env):
     """Custom Environment that follows gym interface"""
@@ -29,48 +28,47 @@ class Pong(gym.Env):
         self.paddleHeight = 100
         self.n_actions = 5
         self.paddleSpeed = 10
-        self.maxscore = 10
         self.observation_space = []
         self.info = {}
-        self.done = False
+        self.done = np.array([False]*num_agents)
         self.action_space = spaces.Discrete(self.n_actions)
         self.observation_space = spaces.Box(low = 0, high = 255, shape = (12,), dtype=np.uint8)
 
     def generateObs(self, ballPos, paddle0Pos, paddle1Pos, ballVel, paddle0Vel, paddle1Vel):
         obs0 = np.array([ballPos, paddle0Pos, paddle1Pos, ballVel, paddle0Vel, paddle1Vel]).flatten()
+        
+        newBallPos = [self.screenWidth - ballPos[0], ballPos[1]]
+        newBallVel = [-ballVel[0], ballVel[1]]
+        newpaddle0Pos = [self.screenWidth - paddle0Pos[0], paddle0Pos[1]]
+        newpaddle1Pos = [self.screenWidth - paddle1Pos[0], paddle1Pos[1]]
+        newpaddle0Vel = [-paddle0Vel[0], paddle0Vel[1]]
+        newpaddle1Vel = [-paddle1Vel[0], paddle1Vel[1]]
 
-        ballPos[0] = self.screenWidth - ballPos[0]
-        ballVel[0] = self.screenWidth - ballVel[0]
-        paddle0Pos[0] = self.screenWidth - paddle0Pos[0]
-        paddle1Pos[0] = self.screenWidth - paddle1Pos[0]
-        paddle0Vel[0] = self.screenWidth - paddle0Vel[0]
-        paddle1Vel[0] = self.screenWidth - paddle1Vel[0]
-
-        obs1 = np.array([ballPos, paddle0Pos, paddle1Pos, ballVel, paddle0Vel, paddle1Vel]).flatten()
+        obs1 = np.array([newBallPos, newpaddle0Pos, newpaddle1Pos, newBallVel, newpaddle0Vel, newpaddle1Vel]).flatten()
         return np.array([obs0, obs1])
 
     def reset(self):
-        self.ball = PongObject([self.screenWidth/2, self.screenHeight/2], [random.choice([1, -1]), 0])
-        self.paddle0 = PongObject([self.paddleWidth, self.screenHeight/2], [0,0])
-        self.paddle1 = PongObject([self.screenWidth - self.paddleWidth, self.screenHeight/2], [0,0])
+        self.ball = PongObject([self.screenWidth/2, self.screenHeight/2], [random.choice([5, -5]), 0])
+        self.paddle0 = PongObject([self.paddleWidth, self.screenHeight/2], [0, 0])
+        self.paddle1 = PongObject([self.screenWidth - self.paddleWidth, self.screenHeight/2], [0, 0])
         if (self.numAgents == 2):
             self.observation = self.generateObs(self.ball.pos, self.paddle0.pos, self.paddle1.pos, self.ball.vel, self.paddle0.vel, self.paddle1.vel)
         elif (self.numAgents == 1):
             self.observation = np.array([self.ball.pos, self.paddle0.pos, self.paddle1.pos, self.ball.vel, self.paddle0.vel, self.paddle1.vel]).flatten()
             self.observation = np.expand_dims(self.observation, axis=0)
+        # print("RESET:")
+        # print("Ball position: ", self.ball.pos)
+        # print("Paddle0 position: ", self.paddle0.pos)
+        # print("Paddle1 position: ", self.paddle1.pos)
+        # print("Ball velocity: ", self.ball.vel)
+        # print("Paddle0 velocity: ", self.paddle0.vel)
+        # print("Paddle1 velocity: ", self.paddle1.vel)
+        # print(" ---------------------- ")
         return self.observation
 
     def step(self, action):
-        # Update velocities
-        if (self.numAgents == 2): 
-            self.paddle0.vel[1] = self.paddleSpeed*(action[0]-2)
-            self.paddle1.vel[1] = self.paddleSpeed*(action[1]-2)
-        elif (self.numAgents == 1):
-            self.paddle0.vel[1] = self.paddleSpeed*(action[0]-2)
-            self.paddle1.vel[1] = self.paddleSpeed*(random.choice([0,1,2,3,4])-2)
-
         # Update positions but keep paddles in the screen
-        self.paddle0.pos[1] = min(max(self.paddle0.vel[0] + self.paddle1.pos[0], 0), self.screenHeight - self.paddleHeight)
+        self.paddle0.pos[1] = min(max(self.paddle0.vel[1] + self.paddle0.pos[1], 0), self.screenHeight - self.paddleHeight)
         self.paddle1.pos[1] = min(max(self.paddle1.vel[1] + self.paddle1.pos[1], 0), self.screenHeight - self.paddleHeight)
 
         # Update ball position, don't let it overlap with paddle or leave the screen
@@ -78,7 +76,7 @@ class Pong(gym.Env):
         meetpaddle1 = False
         meetscreen = False
         newBallX = self.ball.pos[0] + self.ball.vel[0]
-        newBallY = self.ball.pos[0] + self.ball.vel[0]
+        newBallY = self.ball.pos[1] + self.ball.vel[1]
         if (newBallY < 0): 
             newBallY = 0
             meetscreen = True
@@ -94,6 +92,14 @@ class Pong(gym.Env):
             newBallX = self.screenWidth - self.paddleWidth
             meetpaddle1 = True
         self.ball.pos = np.array([newBallX, newBallY])
+
+        # Update velocities
+        if (self.numAgents == 2): 
+            self.paddle0.vel[1] = self.paddleSpeed*(action[0]-2)
+            self.paddle1.vel[1] = self.paddleSpeed*(action[1]-2)
+        elif (self.numAgents == 1):
+            self.paddle0.vel[1] = self.paddleSpeed*(action[0]-2)
+            self.paddle1.vel[1] = self.paddleSpeed*(random.choice([0,1,2,3,4])-2)
 
         # Update ball velocity: hitting the edge of the screen and the paddles will cause it to bounce back
         # Hitting the paddle while the paddle is moving will cause the ball to accelerate in that direction as well
@@ -111,24 +117,29 @@ class Pong(gym.Env):
         if (self.ball.pos[0] < 0):
             reward[1] = 1
             reward[0] = 0
-            self.paddle1.score += 1
+            self.done = np.array([True, True])
+        
         # Player 0 scores a goal
         elif (self.ball.pos[0] > self.screenWidth):
-            reward[1] = 1
-            reward[0] = 0
-            self.paddle0.score += 1
-
-        # If any player scores above maxscore, the game ends
-        if (self.paddle1.score >= self.maxscore or self.paddle0.score >= self.maxscore):
+            reward[1] = 0
+            reward[0] = 1
             self.done = np.array([True, True])
-        else:
-            self.done = np.array([False, False])
 
         if (self.numAgents == 2):
             self.observation = self.generateObs(self.ball.pos, self.paddle0.pos, self.paddle1.pos, self.ball.vel, self.paddle0.vel, self.paddle1.vel)
         elif (self.numAgents == 1):
             self.observation = np.array([self.ball.pos, self.paddle0.pos, self.paddle1.pos, self.ball.vel, self.paddle0.vel, self.paddle1.vel]).flatten()
             self.observation = np.expand_dims(self.observation, axis=0)
+        
+        # print("Ball position: ", self.ball.pos)
+        # print("Paddle0 position: ", self.paddle0.pos)
+        # print("Paddle1 position: ", self.paddle1.pos)
+        # print("Ball velocity: ", self.ball.vel)
+        # print("Paddle0 velocity: ", self.paddle0.vel)
+        # print("Paddle1 velocity: ", self.paddle1.vel)
+        # print("Done: ", self.done)
+        # print("Reward:", reward)
+        # print(" ---------------------- ")
         return self.observation, reward, self.done, self.info
 
     def render(self, mode='human'):
