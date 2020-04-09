110f637b6bb85e49256c66ecaefc251b7e3403f7
Nareg Megan <nmegan@berkeley.edu>
2020-04-08 19:45:47 -0500

    yay

--- symmetric_play/utils/tester.py
+++ symmetric_play/utils/tester.py
@@ -11,27 +11,15 @@ def test(name, num_timesteps, gif=False):
     for i in range(num_timesteps):
         images.append(img)
         action, _ = model.predict(obs)
-        obs, done, reward ,_ = env.step(action)
+        obs, reward, done,_ = env.step(action)
         img = env.render(mode=mode)
-        # if isinstance(done, list):
-        #     done = np.array(done)
-        # if isinstance(done, np.ndarray):
-        #     done = done.any()
-        # if done:
-        #     obs = env.reset()
+        if isinstance(done, list):
+            done = np.array(done)
+        if isinstance(done, np.ndarray):
+            done = done.any()
+        if done:
+            obs = env.reset()
+            break
     file_path = 'output/' + name + '.gif'
     imageio.mimsave(file_path, [np.array(img) for i, img in enumerate(images) if i%2 == 0], fps=29, subrectangles=True)
     env.close()
-
-
-    # images = []
-    # obs = new_env.reset()
-    # img = new_env.render(mode='rgb_array')
-    # for i in range(350):
-    #     images.append(img)
-    #     action, _ = model.predict(obs)
-    #     obs, _, _ ,_ = new_env.step(action)
-    #     img = new_env.render(mode='rgb_array')
-    # file_name = data_dir + name + '.gif'
-    # imageio.mimsave(file_name, [np.array(img) for i, img in enumerate(images) if i%2 == 0], fps=29, subrectangles=True)
-    # new_env.close()
